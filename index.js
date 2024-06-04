89f584b9d55be4e2dfdc9453dbe43abe8d0f2e06
rimakhatun3 <rimamnt46@gmail.com>
2024-06-04 18:00:10 +0600

    added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require("dotenv").config();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
