7041b64ac4b622ae55443f8c2c7191e192c6e7d7
rimakhatun3 <rimamnt46@gmail.com>
2024-06-05 21:17:36 +0600

    update

--- index.js
+++ index.js
@@ -87,13 +87,13 @@ app.post("/jwt", (req,res)=>{
     app.post("/user", async(req,res)=>{
       const user = req.body;
       const isExist = await userCollection.findOne({email: user?.email})
-      if(isExist){
-        return res.send({
-          statu: "success",
-          message: "Login success",
+      // if(isExist){
+      //   return res.send({
+      //     statu: "success",
+      //     message: "Login success",
           
-        })
-      }
+      //   })
+      // }
       const result = await userCollection.insertOne(user)
       res.send(result)
     })
